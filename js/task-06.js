3856e33fa45335b89d8688278c4ad9ddd1f2f5aa
Alexander <sandrix@ukr.net>
2023-01-07 16:02:21 +0200

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -7,6 +7,7 @@ function quantityVerification() {
     input.classList.remove("invalid");
     input.classList.add("valid");
   } else {
+    input.classList.remove("valid");
     input.classList.add("invalid");
   }
 }
